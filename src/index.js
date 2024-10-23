4a868bd2cfc8aa6fee87d4b00d6dc8eba70e246c
thanhan06 <147262467+thanhan06@users.noreply.github.com>
2024-10-23 14:21:48 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv= require("dotenv");
 const { default: mongoose } = require("mongoose");
-const routes = require("./routes")
-const bodyParser = require("body-parser")
+const routes = require('./routes');
+const bodyParser = require("body-parser");
+dotenv.config();
 
-dotenv.config()
+const app=express();
+const port= process.env.PORT || 3001
 
-const app = express()
-const port = process.env.PORT || 3001
+app.get('/',(req,res)=>{
+res.send('Hello World everyone');
+});
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World")
-// })
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 routes(app);
 
 mongoose.connect(`${process.env.MONGO_DB}`)
-    .then(() => {
-        console.log('Connect Database success!')
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-
-app.listen(port, () => {
-    console.log('Server is running in port: ', + port)
+.then(()=>{
+    console.log('Connect Db success!');
 })
+.catch((err)=>{
+    console.log(err);
+});
+
+app.listen(port,()=>{
+    console.log("Server is running in port",+port)
+});
