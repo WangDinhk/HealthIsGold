9538e54743da3acd5bc1dfcc315ee906d089c07b
22120433 <lequangvinh.127.2022@gmail.com>
2024-12-30 21:44:50 +0700

    fix CRUD product, user

--- src/components/AdminProduct/AdminProduct.jsx
+++ src/components/AdminProduct/AdminProduct.jsx
@@ -157,24 +157,15 @@ const AdminProduct = () => {
   };
   //==================================
 
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex
-  ) => {
+  const handleSearch = (selectedKeys, confirm) => {
     confirm();
-    // setSearchText(selectedKeys[0]);
-    // setSearchedColumn(dataIndex);
   };
 
-  const handleReset = (clearFilters: () => void) => {
+  const handleReset = (clearFilters) => {
     clearFilters();
-    // setSearchText('');
   };
 
-  const getColumnSearchProps = (
-    dataIndex: DataIndex
-  ): ColumnType<DataType> => ({
+  const getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
       setSelectedKeys,
       selectedKeys,
@@ -185,18 +176,18 @@ const AdminProduct = () => {
         <InputComponent
           ref={searchInput}
           placeholder={`Search ${dataIndex}`}
-          value={`${selectedKeys[0] || ""}`}
+          value={selectedKeys[0] || ""}
           onChange={(e) =>
             setSelectedKeys(e.target.value ? [e.target.value] : [])
           }
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)} // Loại bỏ as string[]
+          onPressEnter={() => handleSearch(selectedKeys, confirm)}
           style={{ marginBottom: 8, display: "block" }}
         />
 
         <Space>
           <Button
             type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)} // Loại bỏ as string[]
+            onClick={() => handleSearch(selectedKeys, confirm)}
             icon={<SearchOutlined />}
             size="small"
             style={{ width: 90 }}
@@ -214,9 +205,6 @@ const AdminProduct = () => {
         </Space>
       </div>
     ),
-    // filterIcon: (filtered: boolean) => (
-    //   <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-    // ),
     onFilter: (value, record) =>
       record[dataIndex]
         .toString()
@@ -228,17 +216,6 @@ const AdminProduct = () => {
         setTimeout(() => searchInput.current?.select(), 100);
       }
     },
-    // render: text =>
-    //   searchedColumn === dataIndex ? (
-    //     <Highlighter
-    //       highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-    //       searchWords={[searchText]}
-    //       autoEscape
-    //       textToHighlight={text ? text.toString() : ''}
-    //     />
-    //   ) : (
-    //     text
-    //   ),
   });
 
   // ==================================
